0e7c68ec1bf7375638c4a606e43ca6600787afdd
STEVEN BACHIMONT <142581911+stevenbachimont@users.noreply.github.com>
2024-04-08 13:45:38 +0200

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,7 @@ import styles from './Card.module.css';
 function Card() {
     return (
         <div className="card">
-            <img src="./assets/dysto.png" alt="ville seule" />
+            <img src="https://raw.githubusercontent.com/stevenbachimont/card/main/src/assets/dysto.png" alt="ville seule" />
             <h2 className={styles.violetColor}>Dystopy</h2>
             <p>Mourir seul</p>
             <StarRating />
